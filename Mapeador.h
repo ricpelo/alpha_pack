b8183e739d52b9967a86278481ababe77b8ce4f4
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2012-02-13 15:05:11 +0100

    Mejoras en Mapeador.h

--- Mapeador.h
+++ Mapeador.h
@@ -7,6 +7,9 @@ Constant COLOR_LOCAL_MAP  = $ffffff;
 Constant COLOR_CURSOR_MAP = $00ff00;
 Constant COLOR_ACTUAL_MAP = $aaaaaa;
 
+Verb meta 'mapea'
+  *                 -> Mapa;
+
 [ DibujarMapa sitio posx posy central color sep mitad x y;
   if (~~sitio.dibujado) {
     sitio.dibujado = true;
@@ -17,45 +20,45 @@ Constant COLOR_ACTUAL_MAP = $aaaaaa;
     glk_window_fill_rect(gg_mapa_win, color, posx - mitad, posy - mitad,
                                              ladoCuadrado, ladoCuadrado);
     sep = ladoCuadrado + mitad;
-    if (sitio provides e_to && sitio.e_to ofclass Lugar) {
+    if (sitio provides e_to && EsMapeable(sitio.e_to)) {
       glk_window_fill_rect(gg_mapa_win, $ffffff, posx + mitad, posy,
                                                  sep - ladoCuadrado + 1, 1);
       DibujarMapa(sitio.e_to, posx + sep, posy, 0);
     }
-    if (sitio provides w_to && sitio.w_to ofclass Lugar) {
+    if (sitio provides w_to && EsMapeable(sitio.w_to)) {
       glk_window_fill_rect(gg_mapa_win, $ffffff, posx - sep + mitad, posy,
                                                  sep - ladoCuadrado + 1, 1);
       DibujarMapa(sitio.w_to, posx - sep, posy, 0);
     }
-    if (sitio provides n_to && sitio.n_to ofclass Lugar) {
+    if (sitio provides n_to && EsMapeable(sitio.n_to)) {
       glk_window_fill_rect(gg_mapa_win, $ffffff, posx, posy - sep + mitad, 1,
                                                  sep - ladoCuadrado + 1);
       DibujarMapa(sitio.n_to, posx, posy - sep, 0);
     }
-    if (sitio provides s_to && sitio.s_to ofclass Lugar) {
+    if (sitio provides s_to && EsMapeable(sitio.s_to)) {
       glk_window_fill_rect(gg_mapa_win, $ffffff, posx, posy + mitad, 1,
                                                  sep - ladoCuadrado + 1);
       DibujarMapa(sitio.s_to, posx, posy + sep, 0);
     }
-    if (sitio provides nw_to && sitio.nw_to ofclass Lugar) {
+    if (sitio provides nw_to && EsMapeable(sitio.nw_to)) {
       for (x = posx - sep + mitad, y = posy - sep + mitad : x <= posx - mitad : x++, y++) {
         glk_window_fill_rect(gg_mapa_win, $ffffff, x, y, 1, 1);
       }
       DibujarMapa(sitio.nw_to, posx - sep, posy - sep, 0);
     }
-    if (sitio provides ne_to && sitio.ne_to ofclass Lugar) {
+    if (sitio provides ne_to && EsMapeable(sitio.ne_to)) {
       for (x = posx + mitad, y = posy - mitad : x <= posx + sep - mitad : x++, y--) {
         glk_window_fill_rect(gg_mapa_win, $ffffff, x, y, 1, 1);
       }
       DibujarMapa(sitio.ne_to, posx + sep, posy - sep, 0);
     }
-    if (sitio provides sw_to && sitio.sw_to ofclass Lugar) {
+    if (sitio provides sw_to && EsMapeable(sitio.sw_to)) {
       for (x = posx - sep + mitad, y = posy + sep - mitad : x <= posx - mitad : x++, y--) {
         glk_window_fill_rect(gg_mapa_win, $ffffff, x, y, 1, 1);
       }
       DibujarMapa(sitio.sw_to, posx - sep, posy + sep, 0);
     }
-    if (sitio provides se_to && sitio.se_to ofclass Lugar) {
+    if (sitio provides se_to && EsMapeable(sitio.se_to)) {
       for (x = posx + mitad, y = posy + mitad : x <= posx + sep - mitad : x++, y++) {
         glk_window_fill_rect(gg_mapa_win, $ffffff, x, y, 1, 1);
       }
@@ -71,7 +74,12 @@ Constant COLOR_ACTUAL_MAP = $aaaaaa;
   print (name) sitio;
 ];
 
+[ EsMapeable sitio;
+  return sitio ofclass Lugar && sitio has visited;
+];
+
 [ MapaSub ancho alto cenx ceny sitio tecla;
+!  ControlTimer.PausarTimer();
   openGraphicWindow(700);
   gg_mapa_win = gg_bigwin;
   glk_window_get_size(gg_mapa_win, gg_arguments, gg_arguments + WORDSIZE);
@@ -91,19 +99,19 @@ Constant COLOR_ACTUAL_MAP = $aaaaaa;
                   ladoCuadrado = ladoCuadrado - 20;
                   RefrescarMapa(sitio, cenx, ceny);
                 }
-      -5: if (sitio provides s_to && sitio.s_to ofclass Lugar) {
+      -5, '2': if (sitio provides s_to && EsMapeable(sitio.s_to)) {
             sitio = sitio.s_to;
             RefrescarMapa(sitio, cenx, ceny);
           }
-      -4: if (sitio provides n_to && sitio.n_to ofclass Lugar) {
+      -4, '8': if (sitio provides n_to && EsMapeable(sitio.n_to)) {
            sitio = sitio.n_to;
            RefrescarMapa(sitio, cenx, ceny);
           }
-      -2: if (sitio provides w_to && sitio.w_to ofclass Lugar) {
+      -2, '4': if (sitio provides w_to && EsMapeable(sitio.w_to)) {
             sitio = sitio.w_to;
             RefrescarMapa(sitio, cenx, ceny);
           }
-      -3: if (sitio provides e_to && sitio.e_to ofclass Lugar) {
+      -3, '6': if (sitio provides e_to && EsMapeable(sitio.e_to)) {
             sitio = sitio.e_to;
             RefrescarMapa(sitio, cenx, ceny);
           }
@@ -112,6 +120,7 @@ Constant COLOR_ACTUAL_MAP = $aaaaaa;
 .Salir;
   clearMainWindow();
   EncenderGraficosSub(true);
+!  ControlTimer.ReanudarTimer();
   <<Look>>;
 ];
 
