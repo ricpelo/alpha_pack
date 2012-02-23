33eb36bffd2747763584026a8426bfb836f9b34b
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2012-02-23 19:13:17 +0100

    Un espacio menos en el mensaje informativo de Timer.h

--- Timer.h
+++ Timer.h
@@ -18,15 +18,15 @@
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Message " __________________________________________________________________";
-Message "|                                                                  |";
-Message "|                  * TIMER:  I M P O R T A N T E *                 |";
-Message "|                  ===============================                 |";
-Message "| 1. Pon 'Replace KeyDelay;' justo antes de 'Include ~Parser.h~;'  |";
-Message "| 2. Si usas tu propia rutina HandleGlkEvent(),                    |";
-Message "|    no olvides llamar desde esa rutina a:                         |";
-Message "|    ControlTimer.CT_HandleGlkEvent(ev, context, buffer)           |";
-Message "|__________________________________________________________________|";
+Message " _________________________________________________________________";
+Message "|                                                                 |";
+Message "|                  * TIMER:  I M P O R T A N T E *                |";
+Message "|                  ===============================                |";
+Message "| 1. Pon 'Replace KeyDelay;' justo antes de 'Include ~Parser.h~;' |";
+Message "| 2. Si usas tu propia rutina HandleGlkEvent(),                   |";
+Message "|    no olvides llamar desde esa rutina a:                        |";
+Message "|    ControlTimer.CT_HandleGlkEvent(ev, context, buffer)          |";
+Message "|_________________________________________________________________|";
 
 
 ! Nuestra particular versión de HandleGlkEvent:
